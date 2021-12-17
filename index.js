16bd60c93a88584caf9f944c318adee6ec8bd797
Clarisantillan <clarisantillan@icloud.com>
2021-12-16 22:50:40 -0300

    clase2

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1) pensar cual es la solucion
+
+//A hacer clic en el boton
+//B abrir cartel
+//C mostrar el cartel @HOLA
+
+
+//A1 - Identificar el boton
+//A2 - Identificar cuando se hace clic en el boton
+
+//B - hacer algun metodo o funcion para que abra un cartel
+
+//C - introducir un texto que diga hola en el boton
+
+//ID= boton1
+
+// EVENTO= CLIK
+
+//METODO = ALERT()
+
+// TEXTO = HOLA
+
+let boton = document.getElementById()
